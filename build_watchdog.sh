a0669302114ae41c849386a7bb61150167318abe
sneak-it <16104273+sneak-it@users.noreply.github.com>
2026-09-01 17:10:21 -0400

    update compose and local build targets

--- build_watchdog.sh
+++ build_watchdog.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker build -t kinsi55/srcdocks:latest -t kinsi55/srcdocks:watchdog -f watchdog/Dockerfile .
+docker build -t srcdocks:latest -t srcdocks:watchdog -f watchdog/Dockerfile .
